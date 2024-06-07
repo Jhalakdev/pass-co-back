360dd8a69999d993471a3613bb53045839f29ac7
Prateek Kumar <chaudharyprateek1999@gmail.com>
2024-06-07 18:06:41 +0530

    bug fixed

--- models/admin/adminUser.js
+++ models/admin/adminUser.js
@@ -1,54 +1,55 @@
-const mongoose=require("mongoose")
-const jwt=require("jsonwebtoken")
-const adminSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required: false,
-    },
-    email:{
-        type:String,
-        require:true,
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    profilePic: {
-        type: String,
-        required: false,
-    },
-    mobile: {
-        type: String,
-        required: false,
-        unique: true,
-    },
-    token:{
-        type:String,
-    },
-    role:{
-        type:String,
-       default:"ADMIN"
-    },
-    emailotp:{
-        type:String,
-    },
-    mobileotp:{
-        type:String
-    }
-},{timestamps:true})
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-adminSchema.methods.generateAccessToken = function(){
-	return jwt.sign(
-		{
-			_id: this._id,
-		},
-		process.env.ACCESS_TOKEN_SECRET,
-		{
-			expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-		}
-	)
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  profilePic: {
+    type: String,
+    required: false,
+  },
+  mobile: {
+    type: String,
+    required: false,
+  },
+  token: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "ADMIN",
+  },
+  emailotp: {
+    type: String,
+  },
+  mobileotp: {
+    type: String,
   }
+}, { timestamps: true });
 
-const Admin=mongoose.model("Admin",adminSchema);
-module.exports=Admin
+adminSchema.methods.generateAccessToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  );
+}
+
+const Admin = mongoose.model("Admin", adminSchema);
+module.exports = Admin;
