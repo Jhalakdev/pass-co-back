7a47e3aa9889174588384d76d7ae9627e56c4c1f
Prateek Kumar <96932336+Prateek043@users.noreply.github.com>
2024-06-17 14:00:48 +0530

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -1,39 +1,20 @@
-// Import the functions you need from the SDKs you need
-const { initializeApp } =require("firebase/app") ;
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
-const { 
-    getAuth, 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    sendEmailVerification, 
-    sendPasswordResetEmail
-  
-  } = require("firebase/auth") ;
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCrJbkk-3kQAvo2wjj-oQtcMWtBxJEqqs4",
-  authDomain: "passwordmanager-8a388.firebaseapp.com",
-  projectId: "passwordmanager-8a388",
-  storageBucket: "passwordmanager-8a388.appspot.com",
-  messagingSenderId: "527715278975",
-  appId: "1:527715278975:web:d382a35b01f95407159f4a",
-  measurementId: "G-9KZ5GS9C86"
+// Function to verify Google ID token
+const verifyGoogleToken = async (idToken) => {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    return decodedToken;
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    throw error;
+  }
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-module.exports = {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signOut,
-    sendEmailVerification,
-    sendPasswordResetEmail,
-
-  };
+module.exports = { verifyGoogleToken };
